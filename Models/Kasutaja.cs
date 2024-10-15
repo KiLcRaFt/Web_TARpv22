f954319f187ddcc572a2a2323bd0c23ede4e70ee
opilane <opilane@HP-CZC2349HT2>
2024-10-15 14:37:52 +0300

    Add project files.

---
+++ Models/Kasutaja.cs
@@ -0,0 +1,21 @@
+﻿namespace Web_TARpv22.Models
+{
+    public class Kasutaja
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Nimi { get; set; }
+        public string Perenimi { get; set; }
+
+
+        public Kasutaja(int id, string username, string password, string nimi, string perenimi)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Nimi = nimi;
+            Perenimi = perenimi;
+        }
+    }
+}
